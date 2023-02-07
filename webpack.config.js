fcd29940db1516a71d64715b2aed099457640912
yan160100 <1909484942@qq.com>
2023-02-07 15:21:18 +0800

    更新webpack.config.js配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// import HtmlWebpackPlugin from 'html-webpack-plugin'//与上面一行等价
+const path = require('path') // path模块主要是为了解决绝对路径问题的，就是他要找的这个文件必须得有一个绝对路径，但你自己写的绝对路径肯定就很麻烦，所以我们可以使用path下面的resolve()方法
+const MiniClassExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
 
 module.exports = {
   mode: 'production',
@@ -18,4 +23,16 @@ module.exports = {
       },
     ],
   },
+  plugins:[ // 装的是实例化对象
+  new HtmlWebpackPlugin({
+      filename:'index.html',//打包后的filename是什么
+      template:path.resolve(__dirname,'./public/index.html'),//打包的是谁
+      chunks:['index'],
+      // chunks:['jquery','index'],//入口文件是哪一个，可以写多个入口文件
+      excludeChunks:['node_modules'] // 需要排除哪个文件
+  }),
+  new MiniClassExtractPlugin({
+      filename: 'styles/[contenthash].css',// 打包的css放到styles文件夹下面（styles文件夹会自己创建）
+  })
+],
 };
